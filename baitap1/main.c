22fe3fa74c71dbf88e276c9234a0cf0b2fee09ab
Anhdao1806 <daoleho123@gmail.com>
2024-12-16 19:30:34 +0700

    Add files via upload

---
+++ baitap1/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>;
+int main(){
+    int n;
+    printf ("Nhap so");
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf("%d la so am \n",n);
+    }
+    else {
+    printf("%d la so duong \n",n);
+}
+    return 0;
+}
+
